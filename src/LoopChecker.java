39662be02c67c5199b2ed5efaa798609cebe7be0
Nikita <athlon64x2600@gmail.com>
2022-11-30 17:24:43 +0200

    Homework Exceptions

---
+++ src/LoopChecker.java
@@ -0,0 +1,18 @@
+public class LoopChecker implements Checker {
+
+    private static final String VALID_CHARACTERS = "0123456789_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    @Override
+    public void check(String s, boolean login) throws WrongLoginException, WrongPasswordException {
+        for (int i = 0; i < s.length(); i++) {
+            if(!VALID_CHARACTERS.contains(String.valueOf(s.charAt(i)))) {
+                if(login){
+                    throw new WrongLoginException("Логин должен содержать только символы латинского алфавита, цифры или знак подчёркивания");
+                }else{
+                    throw new WrongPasswordException("Пароль должен содержать только символы латинского алфавита, цифры или знак подчёркивания");
+                }
+
+            }
+        }
+    }
+}
